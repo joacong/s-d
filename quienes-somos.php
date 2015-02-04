d75ea767f7f81ba648d784f66d6f487419ddad30
Joaquin Nuñez <nunezg.ja@gmail.com>
2015-02-04 13:33:11 -0300

    Add quienes somos

---
+++ quienes-somos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php include('layout/head.html') ?>
+<body>
+  <?php include('layout/navbar.html') ?>
+    <div class="container">
+      <div class="page-header" id="banner">
+        <div class="row">
+          <div class="col-lg-8 col-md-7 col-sm-6">
+            <h1>Quienes somos</h1>
+            <p class="lead">S&D es una empresa....</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php include('layout/footer.html') ?>
+</body>
+</html>
